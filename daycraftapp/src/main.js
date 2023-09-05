ee541db10e1fa78f3a9e25c8633eee989278b15d
emna mj <emnamejeri23@gmail.com>
2023-09-05 09:10:59 +0300

    final commit

--- daycraftapp/src/main.js
+++ daycraftapp/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-
-createApp(App).mount("#app");
+import router from './router';
+createApp(App).use(router).mount("#app");
